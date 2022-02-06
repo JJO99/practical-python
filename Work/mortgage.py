e1ebe56c1371dbab2f8c6e5442827dbfc7ac46b2
juno <justjjo@koreatech.ac.kr>
2022-02-06 19:17:53 +0900

    1.3 숫자 연습문제 1.9 / 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -4,15 +4,22 @@ payment = 2684.11
 total_paid = 0.0
 month = 0
 
-for x in range(12):
-    principal = principal * (1 + rate / 12) - (payment + 1000)
-    total_paid = total_paid + (payment + 1000)
-    month += 1
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
-    principal = principal * (1 + rate / 12) - payment
-    total_paid = total_paid + payment
-    month += 1
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal = principal * (1 + rate / 12) - (payment + extra_payment)
+        total_paid = total_paid + (payment + extra_payment)
+        month += 1
+
+    elif month < extra_payment_start_month or extra_payment_end_month < month:
+        principal = principal * (1 + rate / 12) - payment
+        total_paid = total_paid + payment
+        month += 1
+
+    print(month, round(total_paid, 2), round(principal, 2))
 
 print('Total paid', round(total_paid, 2))
-print('소요 월 수', month)
+print('Months', month)
