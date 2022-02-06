a94e5842dc4b5b9267ca494f28a0135c69b82e79
juno <justjjo@koreatech.ac.kr>
2022-02-06 18:53:55 +0900

    1.3 숫자 연습문제 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,9 +2,17 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month = 0
+
+for x in range(12):
+    principal = principal * (1 + rate / 12) - (payment + 1000)
+    total_paid = total_paid + (payment + 1000)
+    month += 1
 
 while principal > 0:
     principal = principal * (1 + rate / 12) - payment
     total_paid = total_paid + payment
+    month += 1
 
-print('Total paid', total_paid)
+print('Total paid', round(total_paid, 2))
+print('소요 월 수', month)
