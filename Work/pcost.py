d29934b3a4341fcd3adb217cb28a71916f9d7fe2
juno <justjjo@koreatech.ac.kr>
2022-02-08 21:28:07 +0900

    1.6 파일 관리 연습문제 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
-# pcost.py
-#
-# Exercise 1.27
+total = 0
+
+with open('./Data/portfolio.csv', 'rt') as file:
+    next(file)
+    for line in file:
+        row = line.split(',')
+        row[2] = row[2].strip('\n')
+        total += int(row[1]) * float(row[2])
+
+print('Total cost: ', total)
