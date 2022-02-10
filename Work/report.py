99ff7464d69974c9edcc2ee7c70489677f66f5ca
juno <justjjo@koreatech.ac.kr>
2022-02-10 21:52:12 +0900

    2.2 컨테이너 연습문제 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
-# report.py
-#
-# Exercise 2.4
+import csv
+
+
+def read_portfolio(filename):
+    portfolio = []
+
+    f = open(filename)
+    rows = csv.reader(f)
+    next(rows)
+
+    for row in rows:
+        holding = (row[0], int(row[1]), float(row[2]))
+        portfolio.append(holding)
+
+    return portfolio
+
+### 인터프리터에서 portfolio = read_portfolio('Data/portfolio.csv') 실행
+### total = 0.0
+### for name, shares, price in portfolio:
+###     total += shares*price
