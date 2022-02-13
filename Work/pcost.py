9dc1c84b53d123afbbc13561e58e570526e2bdef
juno <justjjo@koreatech.ac.kr>
2022-02-13 14:16:06 +0900

    2.4 시퀸스 연습문제 2.15 실용적인 enumerate() 예제 missing.csv를 활용함

--- Work/pcost.py
+++ Work/pcost.py
@@ -9,12 +9,11 @@ def portfolio_cost(filelink):
     f = open(filelink)
     rows = csv.reader(f)
     next(rows)
-    for line in rows:
-        checkLine += 1
-        if line[0] == '' or line[1] == '' or line[2] == '':
-            print(f'Line {checkLine} Pass')
-            continue
-        total += int(line[1]) * float(line[2])
+    for count, line in enumerate(rows, start=1):
+        try:
+            total += int(line[1]) * float(line[2])
+        except ValueError:
+            print(f'Row {count}: Error Occurred - {line}')
 
     return total
 
@@ -22,12 +21,7 @@ def portfolio_cost(filelink):
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
-    filename = './Data/portfolio.csv'
+    filename = './Data/missing.csv'
 
 cost = portfolio_cost(filename)
 print('Total cost:', cost)
-
-### 이 코드는
-### python3 /Users/(계정명)/PycharmProjects/practical-python/Work/pcost.py /Users/(계정명)/PycharmProjects/practical-python/Work/Data/portfolio.csv
-### 을 터미널에서 실행해야 한다.
-### 파이참에서 그냥 실행하면 결과가 주르륵 나오고, 터미널에서 세번째 인자 없이 실행하면 오류 발생?
