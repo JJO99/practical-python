946dbd4bdc98176e0e7b008b8cd021fcd0b6c820
juno <justjjo@koreatech.ac.kr>
2022-02-15 20:31:39 +0900

    3.2 함수의 작동 연습문제 3.6

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,17 +1,21 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None, has_header=None):
+def parse_csv(filename, select=None, types=None, has_header=True):
     '''
     CSV 파일을 파싱해 레코드의 목록을 생성한다.
     '''
     with open(filename) as f:
         rows = csv.reader(f)
 
-        headers = next(rows)   # 헤더를 읽는다.
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select   # 원하는 헤더만 읽는다.
+        if has_header:
+            headers = next(rows)  # 헤더를 읽는다.
+
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select  # 원하는 헤더만 읽는다.
+            else:
+                indices = []
         else:
             indices = []
 
@@ -21,14 +25,18 @@ def parse_csv(filename, select=None, types=None, has_header=None):
             if not row:  # 데이터가 없는 행은 건너뛴다.
                 continue
 
-            if indices:
+            if indices:   # 헤더 선택 여부에 따라
                 row = [row[index] for index in indices]
 
-            if types:
-                conv = [func(val) for func, val in zip(types, row)]   # 형변환
-                record = dict(zip(headers, conv))
+            if types:   # 형변환 여부에 따라
+                conv = [func(val) for func, val in zip(types, row)]
             else:
-                record = dict(zip(headers, row))
+                conv = row   # 형변환 안해도 conv에 집어넣기
+
+            if not has_header:   # 헤더 여부에 따라 _ 없으면 리스트 있으면 딕셔너리
+                record = list(conv)
+            else:
+                record = dict(zip(headers, conv))
 
             records.append(record)
 
