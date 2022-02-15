632e7f04f6cd66cd9f8eb438776208d2031b57c8
juno <justjjo@koreatech.ac.kr>
2022-02-15 20:05:50 +0900

    3.2 함수의 작동 연습문제 3.4 + 제작자의 답으로 변경

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -10,11 +10,19 @@ def parse_csv(filename, select=None):
 
         headers = next(rows)   # 헤더를 읽는다.
         if select:
-            headers = select
+            indices = [headers.index(colname) for colname in select]
+            headers = select   # 원하는 헤더만 읽는다.
+        else:
+            indices = []
+
         records = []
         for row in rows:
             if not row:  # 데이터가 없는 행은 건너뛴다.
                 continue
+
+            if indices:
+                row = [row[index] for index in indices]
+
             record = dict(zip(headers, row))
             records.append(record)
 
