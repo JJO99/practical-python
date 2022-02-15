ee0abb9397389904349d12ae007de7c04d42bd2e
juno <justjjo@koreatech.ac.kr>
2022-02-15 19:59:39 +0900

    3.2 함수의 작동 연습문제 3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,19 @@
-# fileparse.py
-#
-# Exercise 3.3
+import csv
+
+
+def parse_csv(filename):
+    '''
+    CSV 파일을 파싱해 레코드의 목록을 생성한다.
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        headers = next(rows)   # 헤더를 읽는다.
+        records = []
+        for row in rows:
+            if not row:  # 데이터가 없는 행은 건너뛴다.
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+
+        return records
