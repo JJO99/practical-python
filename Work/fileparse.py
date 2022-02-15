9d5192f3d7def1538128494ee9a7d8b7fad0d80a
juno <justjjo@koreatech.ac.kr>
2022-02-15 21:19:09 +0900

    3.3 오류 검사 연습문제 3.9

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     '''
     CSV 파일을 파싱해 레코드의 목록을 생성한다.
@@ -25,23 +24,32 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             indices = []
 
         records = []
+        count = 0
 
         for row in rows:
-            if not row:  # 데이터가 없는 행은 건너뛴다.
+            try:
+                if not row:  # 데이터가 없는 행은 건너뛴다.
+                    continue
+
+                if indices:  # 헤더 선택 여부에 따라
+                    row = [row[index] for index in indices]
+
+                if types:  # 형변환 여부에 따라
+                    conv = [func(val) for func, val in zip(types, row)]
+                else:
+                    conv = row  # 형변환 안해도 conv에 집어넣기
+
+                if not has_headers:  # 헤더 여부에 따라 _ 없으면 리스트 있으면 딕셔너리
+                    record = list(conv)
+                    count += 1
+                else:
+                    record = dict(zip(headers, conv))
+                    count += 1
+            except ValueError as e:
+                count += 1
+                print(f'Row {count}: Couldn"t convert {row}')
+                print(f'Row {count}: {e}')
                 continue
-
-            if indices:  # 헤더 선택 여부에 따라
-                row = [row[index] for index in indices]
-
-            if types:  # 형변환 여부에 따라
-                conv = [func(val) for func, val in zip(types, row)]
-            else:
-                conv = row  # 형변환 안해도 conv에 집어넣기
-
-            if not has_headers:  # 헤더 여부에 따라 _ 없으면 리스트 있으면 딕셔너리
-                record = list(conv)
-            else:
-                record = dict(zip(headers, conv))
 
             records.append(record)
 
