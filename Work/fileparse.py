64c61f40bb0d2b5632f02e3f8c14b99343ca9b90
juno <justjjo@koreatech.ac.kr>
2022-02-15 21:08:02 +0900

    3.3 오류 검사 연습문제 3.8

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -6,6 +6,10 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
     CSV 파일을 파싱해 레코드의 목록을 생성한다.
     '''
     with open(filename) as f:
+        if not has_headers:
+            if select:
+                raise RuntimeError("헤더가 없는데 선택을 어떻게 하죠?")
+
         rows = csv.reader(f, delimiter=delimiter)
 
         if has_headers:
@@ -16,6 +20,7 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
                 headers = select  # 원하는 헤더만 읽는다.
             else:
                 indices = []
+
         else:
             indices = []
 
@@ -25,15 +30,15 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             if not row:  # 데이터가 없는 행은 건너뛴다.
                 continue
 
-            if indices:   # 헤더 선택 여부에 따라
+            if indices:  # 헤더 선택 여부에 따라
                 row = [row[index] for index in indices]
 
-            if types:   # 형변환 여부에 따라
+            if types:  # 형변환 여부에 따라
                 conv = [func(val) for func, val in zip(types, row)]
             else:
-                conv = row   # 형변환 안해도 conv에 집어넣기
+                conv = row  # 형변환 안해도 conv에 집어넣기
 
-            if not has_headers:   # 헤더 여부에 따라 _ 없으면 리스트 있으면 딕셔너리
+            if not has_headers:  # 헤더 여부에 따라 _ 없으면 리스트 있으면 딕셔너리
                 record = list(conv)
             else:
                 record = dict(zip(headers, conv))
